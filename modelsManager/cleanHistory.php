86a0f2780a416f145a6a5f19b1bcd6707ec68b20
Michel Blockelet <blockelet@crans.org>
2020-09-06 04:15:40 +0200

    cleanHistory: New version

--- modelsManager/cleanHistory.php
+++ modelsManager/cleanHistory.php
@@ -1,31 +1,53 @@
 <?php
 
-/* Copyright (c) 2013 Apycat / Association France-ioi, MIT License http://opensource.org/licenses/MIT */
+/* Copyright (c) 2013-2020 Apycat / Association France-ioi, MIT License http://opensource.org/licenses/MIT */
+
+die("No access.");
 
 require_once __DIR__."/../../shared/connect.php";
 require_once __DIR__."/../../shared/models.php";
 
-echo "Cleaning all history tables:<br/>";
-foreach ($tablesModels as $tableName => $tableModel) {
-   $query = "truncate history_$tableName;";
-   echo $query."<br/>";
-   $db->exec($query);
-}
 
-echo "<br/>Regenerating minimal history :<br/>";
-foreach ($tablesModels as $tableName => $tableModel) {
+function cleanHistory($tableName, $tableModel) {
+   global $db;
+
+   echo " * Table $tableName\n";
+   $historyTableName = 'history_' . $tableName;
+
+   echo "Locking tables : ";
+   $db->exec("LOCK TABLES `$tableName` WRITE, `$historyTableName` WRITE;");
+   echo " done.\n";
+
+   echo "Cleaning history table for $tableName... ";
+   $db->exec("TRUNCATE `$historyTableName`;");
+   echo " done.\n";
+
+   echo "Regenerating minimal history :\n";
    $fields = array_keys($tableModel['fields']);
+   array_unshift($fields, 'ID');
+   $fields[] = 'iVersion';
    $fieldsStr = "`".implode('`, `', $fields)."`";
    $fieldsStrWithPrefix = "`".$tableName."`.`".implode("`, `".$tableName."`.`", $fields)."`";
 
-   $query = "INSERT INTO `history_".$tableName."` (`ID`, ".$fieldsStr.", `bDeleted`, `iVersion`, `iNextVersion`) ".
-      "(SELECT `".$tableName."`.`ID`, ".$fieldsStrWithPrefix.", 0 as `bDeleted`, CURRENT_TIMESTAMP as `iVersion`, NULL as `iNextVersion` ".
-       "FROM `".$tableName."` ".
-       "LEFT JOIN `history_".$tableName."` ON (`history_".$tableName."`.`ID` = `".$tableName."`.`ID` AND `history_".$tableName."`.`bDeleted` IS NULL AND `history_".$tableName."`.`iNextVersion` IS NULL)".
-       "WHERE `history_".$tableName."`.`ID` IS NULL)";
+   $query = "INSERT INTO `$historyTableName` (".$fieldsStr.", `bDeleted`, `iNextVersion`) ".
+      "(SELECT ".$fieldsStrWithPrefix.", 0 as `bDeleted`, NULL as `iNextVersion` ".
+       "FROM `$tableName`)";
 
-   
-   echo $tableName;
+   echo $query . "\n";
    $db->exec($query);
-   echo " ok<br/>";
+   echo "done.\n";
+
+   $db->exec("UNLOCK TABLES;");
+}
+
+$tableNames = [];
+foreach ($tablesModels as $tableName => $tableModel) {
+   $tableNames[] = $tableName;
+   if($argc > 1 && ($argv[1] == '--all' || in_array($tableName, $argv))) {
+      cleanHistory($tableName, $tableModel);
+   }
+}
+if($argc < 2) {
+   echo "Specify table names or '--all' for all tables.\n";
+   echo "Tables with history : " . implode(' ', $tableNames) . "\n";
 }
